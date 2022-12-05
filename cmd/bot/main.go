f6a0a2d035a413c1d5047ba96322fe005bd2e536
Alex <alexander_filippov@hotmail.com>
2022-12-05 04:07:18 +0100

    initial

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"log"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	dsn := "postgresql://bot_dev:s1mOH8z9FkSK3i4abAkOiA@gamma-lemur-6172.8nj.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full"
+	ctx := context.Background()
+	conn, err := pgxpool.New(ctx, dsn)
+	defer conn.Close()
+	if err != nil {
+		log.Fatal("failed to connect database", err)
+	}
+
+	var now time.Time
+	err = conn.QueryRow(ctx, "SELECT NOW()").Scan(&now)
+	if err != nil {
+		log.Fatal("failed to execute query", err)
+	}
+
+	fmt.Println(now)
 }
